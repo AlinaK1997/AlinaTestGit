5492836da95569bda4773c7c8854fb60d83ddbbb
AlinaK1997 <alinakompanets27@gmail.com>
2024-11-27 20:22:52 +0100

    Quiz File Alina

---
+++ Quiz Sprint.py
@@ -0,0 +1,34 @@
+print("Quiz informatica")
+
+# menu_string = """
+#    1: Question
+#     1. option
+#     2. option
+#     3. option\n"""
+
+# selection = int(input(menu_string))
+
+
+# if selection == 1:
+#     print("Wrong answer")
+
+# elif selection == 2:
+#     print("Wrong answer")
+    
+# elif selection == 3:
+#     print("Right answer")
+
+# else:
+#     print("invalid input")
+
+
+Q1 = "xxxxxx"
+Q2 = "xxxxxx"
+Q3 = "xxxxxx"
+Q4 = "xxxxxx"
+Q5 = "xxxxxx"
+lista_domande = [Q1, Q2, Q3, Q4, Q5]
+lista_risposte_giuste = []
+
+from random import randint
+print(lista_domande.randint)
